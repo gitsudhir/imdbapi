edef214285a0e78704a29650837d0cbe44e8c182
sudhir <sudhir@workplacefabric.com>
2022-07-10 14:43:22 +0530

    database connected

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
+const pgp = require('pg-promise')(/* options */)
 const express = require('express')
+const { Client } = require('pg')
+require('dotenv').config()
+
+let ssl = null;
+if (process.env.NODE_ENV === 'development') {
+	ssl = { rejectUnauthorized: false };
+}
+const config = {
+	connectionString: process.env.DATABASE_URL_DB,
+	max: 30,
+	ssl: ssl
+};
 const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
 	res.send('Hello World by sudhir kumar!')
+})
+app.get('/api', (req, res) => {
+
+	const db = pgp(config);
+
+	db.many('SELECT * FROM todolist')
+		.then((data) => {
+			console.log('DATA:', data)
+			res.send(JSON.stringify(data));
+		})
+		.catch((error) => {
+			console.log('ERROR:', error)
+			res.status(500).send('Something broke!')
+		})
+
 })
 
 app.listen(port, () => {
